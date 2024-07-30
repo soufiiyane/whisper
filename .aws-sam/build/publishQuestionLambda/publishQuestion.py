0528d291a6a5f9d7138e7e1ae0f3e67aa570f1db
soufiiyane <soufiyane.aitmoulay@gmail.com>
2024-07-30 03:27:06 +0100

    adding publish question functionality, still needs some improvements

--- .aws-sam/build/publishQuestionLambda/publishQuestion.py
+++ .aws-sam/build/publishQuestionLambda/publishQuestion.py
@@ -1,15 +1,57 @@
-import json
-import datetime
 import boto3
+import json
+import os
+
 
 def lambda_handler(event, context):
+
     try:
+        client = boto3.client('cognito-idp')
+        user_pool_id = os.environ['USER_POOL_ID']
+
+        if 'body' not in event or event['httpMethod'] != 'POST':
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'msg': 'Bad Request'})
+            }
+
+        body = json.loads(event["body"])
+        if 'uuid' not in body:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'msg': 'UUID field is missing'})
+            }
+        uuid = body["uuid"]
+        response = client.admin_get_user(
+            UserPoolId=user_pool_id,
+            Username=uuid
+        )
+
         return {
             'statusCode': 200,
-            'body': json.dumps({'msg': "Question successfully published"})
+            'body': json.dumps({
+                'Username': response['Username'],
+                'Attributes': {attr['Name']: attr['Value'] for attr in response['UserAttributes']}
+            }),
+            'headers': {
+                'Content-Type': 'application/json'
+            }
+        }
+
+    except client.exceptions.UserNotFoundException:
+        return {
+            'statusCode': 404,
+            'body': json.dumps({'error': 'User not found'}),
+            'headers': {
+                'Content-Type': 'application/json'
+            }
         }
     except Exception as e:
+        # Handle other exceptions
         return {
             'statusCode': 500,
-            'body': json.dumps({'msg': 'Internal server error', 'error': str(e)})
+            'body': json.dumps({'error': str(e)}),
+            'headers': {
+                'Content-Type': 'application/json'
+            }
         }
--- src/publishQuestion.py
+++ src/publishQuestion.py
@@ -1,15 +1,73 @@
-import json
-import datetime
 import boto3
+import json
+import os
+import datetime
+
 
 def lambda_handler(event, context):
+    cognito_client = boto3.client('cognito-idp')
+    dynamodb_client = boto3.resource('dynamodb')
+
+    user_pool_id = os.environ['USER_POOL_ID']
+    table_name = "questions"
+
     try:
+        if 'body' not in event:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'msg': 'Bad Request'})
+            }
+
+        body = json.loads(event["body"])
+        if 'uuid' not in body:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'msg': 'UUID field is missing'})
+            }
+        uuid = body["uuid"]
+
+        response = cognito_client.admin_get_user(
+            UserPoolId=user_pool_id,
+            Username=uuid
+        )
+
+        username = response['Username']
+        attributes = {attr['Name']: attr['Value'] for attr in response['UserAttributes']}
+        family_name = attributes.get('family_name', 'Unknown')
+        given_name = attributes.get('given_name', 'Unknown')
+
+        question_item = {
+            'userName': username,
+            'question': "question me",
+            'Date': str(datetime.datetime.now())
+        }
+        table_questions = dynamodb_client.Table(table_name)
+
+        table_questions.put_item(Item=question_item)
+
         return {
             'statusCode': 200,
-            'body': json.dumps({'msg': "Question successfully published"})
+            'body': json.dumps({
+                'msg': f'Your question has been published successfully to user {family_name} {given_name}'
+            }),
+            'headers': {
+                'Content-Type': 'application/json'
+            }
+        }
+
+    except cognito_client.exceptions.UserNotFoundException:
+        return {
+            'statusCode': 404,
+            'body': json.dumps({'error': 'User not found'}),
+            'headers': {
+                'Content-Type': 'application/json'
+            }
         }
     except Exception as e:
         return {
             'statusCode': 500,
-            'body': json.dumps({'msg': 'Internal server error', 'error': str(e)})
+            'body': json.dumps({'error': str(e)}),
+            'headers': {
+                'Content-Type': 'application/json'
+            }
         }
